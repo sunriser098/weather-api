36a7f286696e25311cf8580fafa4039a5cae9058
sunriser <sunriser098@gmail.com>
2024-03-23 10:47:10 -0500

    Initial Commit.

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
+import pandas as pd
 from flask import Flask, render_template
 
+# create the website object
 app = Flask(__name__)
 
 
+# default page template
 @app.route("/")
 def home():
     return render_template("home.html")
 
 
 @app.route("/api/v1/<station>/<date>")
-def about(station, date):
+def data(station, date):
     temperature = 23
     return {"station": station,
             "date": date,
             "temperature": temperature}
 
 
+# run app
 if __name__ == "__main__":
     app.run(debug=True)
-
-
