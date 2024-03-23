1fcc559e30151d0eee43150172da55716dc41c0a
sunriser <sunriser098@gmail.com>
2024-03-23 13:01:17 -0500

    Added data files and return temperatue for station and date.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 from flask import Flask, render_template
 
 # create the website object
@@ -13,7 +14,9 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def data(station, date):
-    temperature = 23
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze()/10
     return {"station": station,
             "date": date,
             "temperature": temperature}
